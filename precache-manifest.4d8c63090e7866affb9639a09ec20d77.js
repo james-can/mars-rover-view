618e6d45200aaa02e051ee338a6e2dfbfaa190e1
James Canada <jamesccanada@gmail.com>
2019-05-15 14:52:01 -0500

    Updates

---
+++ precache-manifest.4d8c63090e7866affb9639a09ec20d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b18770c3f507f9d19056",
+    "url": "/mars-rover-view/static/js/runtime~main.9d785713.js"
+  },
+  {
+    "revision": "d8e5d62a9b436e4e352a",
+    "url": "/mars-rover-view/static/js/main.9b50791b.chunk.js"
+  },
+  {
+    "revision": "c2c26c48cf3d73c1d540",
+    "url": "/mars-rover-view/static/js/2.24c70300.chunk.js"
+  },
+  {
+    "revision": "d8e5d62a9b436e4e352a",
+    "url": "/mars-rover-view/static/css/main.92e02741.chunk.css"
+  },
+  {
+    "revision": "7b315c30e74e19a13a3fe7eba0e77bbf",
+    "url": "/mars-rover-view/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mars-rover-view/precache-manifest.9b35b8e0c2dbe426f27eb8ea35420616.js"
+  "/mars-rover-view/precache-manifest.4d8c63090e7866affb9639a09ec20d77.js"
 );
 
 workbox.clientsClaim();
